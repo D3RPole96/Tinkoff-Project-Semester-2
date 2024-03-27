b8e574d0506e9d4082b4af21a03af467e4d92f08
D3RPole96 <ilya.lagunov2003@gmail.com>
2024-03-27 23:18:36 +0500

    Project init

---
+++ src/main/java/edu/example/tinkoffprojectsemester2/TinkoffProjectSemester2Application.java
@@ -0,0 +1,13 @@
+package edu.example.tinkoffprojectsemester2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TinkoffProjectSemester2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TinkoffProjectSemester2Application.class, args);
+    }
+
+}
